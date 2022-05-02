a5767ec02ac21058065ff527e2ba61f8b114554e
harutas <96802323+harutas@users.noreply.github.com>
2022-05-03 01:33:22 +0900

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,153 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <v-app>
+        <div class="d-flex flex-nowrap overflow-auto pb-3">
+            <div class="d-flex flex-column">
+                <div class="d-flex">
+                    <v-form
+                    ref="form"
+                    class="d-flex"
+                    >
+                        <v-text-field
+                          v-model="sectionName"
+                          label="Section"
+                          required
+                          class="ma-3"
+                        >
+                        </v-text-field>
+                        <v-btn
+                          class="mt-5"
+                          @click="createSection"
+                        >
+                          ADD SECTION
+                        </v-btn>
+                    </v-form>   
+            </div>
+                <div class="d-flex align-start">
+                    <task-section
+                    v-on:delete-section="deleteSection(index)"
+                    v-on:create-task="createTask"
+                    v-on:delete-task="deleteTask"     
+                    v-on:input="changeSectionName"
+                    v-on:change-task-title="changeTaskTitle"
+                    v-on:change-task-content="changeTaskContent"
+                    v-on:toggle-can-edit="toggleCanEdit"
+                    v-on:toggle-is-done="toggleIsDone"
+                    v-on:toggle-is-favorite="toggleIsFavorite"        
+                    v-for="(section, index) in sectionArray"
+                    :section="section"
+                    :key="section.sectionId"></task-section>
+                </div>
+            </div>
+        </div>
+    </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TaskSection from './components/TaskSection.vue';
+import {Section} from '/src/task.js';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+    name: 'App',
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+    components: {
+        TaskSection
+    },
+
+    data(){
+        return {
+            sectionId : 0,
+            sections : [],
+            sectionName : ""
+        } 
+    },
+
+    methods : {
+        createSection(){
+            let section = new Section(this.sectionId, this.sectionName);
+            this.sectionId++;
+            console.log(this.sections);
+            return this.sections.push(section);
+        },
+
+        deleteSection(index){
+            this.$delete(this.sections, index);
+        },
+
+        changeSectionName($event, sectionId){
+            const identifiedSection = this.sections.find(section => section.sectionId === sectionId);
+            identifiedSection.sectionName = $event;
+        },
+
+        createTask(sectionId, taskObj){
+            const identifiedSection = this.sections.find(section => section.sectionId === sectionId);
+            identifiedSection.taskArray.push(taskObj);
+        },
+
+        deleteTask(sectionId, taskId){
+            const identifiedSection = this.sections.find(section => section.sectionId === sectionId);
+            const taskArr = identifiedSection.taskArray;
+            for (let i = 0; i < taskArr.length; i++){
+                if (taskArr[i].taskId === taskId) {
+                    taskArr.splice(i, 1);
+                }
+            }
+        },
+
+        changeTaskTitle($event, sectionId, taskId){
+            const identifiedSection = this.sections.find(section => section.sectionId === sectionId);
+            const taskArr = identifiedSection.taskArray;
+            for (let i = 0; i < taskArr.length; i++){
+                if (taskArr[i].taskId === taskId) {
+                    taskArr[i].title = $event;
+                }
+            }
+        },
+
+        changeTaskContent($event, sectionId, taskId){
+            const identifiedSection = this.sections.find(section => section.sectionId === sectionId);
+            const taskArr = identifiedSection.taskArray;
+            for (let i = 0; i < taskArr.length; i++){
+                if (taskArr[i].taskId === taskId) {
+                taskArr[i].content = $event;
+                }
+            }
+        },
+
+        toggleCanEdit(canEdit, sectionId, taskId){
+            const identifiedSection = this.sections.find(section => section.sectionId === sectionId);
+            const taskArr = identifiedSection.taskArray;
+            for (let i = 0; i < taskArr.length; i++){
+                if (taskArr[i].taskId === taskId) {
+                taskArr[i].canEdit = !canEdit;
+                }
+            }
+        },
+
+        toggleIsDone(isDone, sectionId, taskId){
+            const identifiedSection = this.sections.find(section => section.sectionId === sectionId);
+            const taskArr = identifiedSection.taskArray;
+            for (let i = 0; i < taskArr.length; i++){
+                if (taskArr[i].taskId === taskId) {
+                    taskArr[i].isDone = !isDone;
+                }
+            }
+        },
+
+        toggleIsFavorite(isFavorite, sectionId, taskId){
+            const identifiedSection = this.sections.find(section => section.sectionId === sectionId);
+            const taskArr = identifiedSection.taskArray;
+            for (let i = 0; i < taskArr.length; i++){
+                if (taskArr[i].taskId === taskId) {
+                taskArr[i].isFavorite = !isFavorite;
+                }
+            }
+        },
+    },
+
+    computed : {
+        sectionArray(){
+            return this.sections;
+        }
+    }
+};
+</script>
---
+++ src/components/TaskSection.vue
@@ -0,0 +1,102 @@
+<template>
+    <div class="mx-3">
+        <v-card width="250" class="pa-1">
+        <!-- セクション名 -->
+            <v-text-field
+                v-model="sectionName"
+                @input="$emit('input', $event, section.sectionId)"
+                label="Section"
+                hide-details="false"
+            ></v-text-field>
+            <task-card
+                v-on:delete-task="deleteTask"
+                v-on:change-task-title="changeTaskTitle"
+                v-on:change-task-content="changeTaskContent"
+                v-on:toggle-can-edit="toggleCanEdit"
+                v-on:toggle-is-done="toggleIsDone"
+                v-on:toggle-is-favorite="toggleIsFavorite"
+                :sectionName="sectionName"
+                :section="section"
+                v-for="task in section.taskArray"
+                :task="task"
+                :key="task.taskId">
+            </task-card>
+            <div class="d-flex justify-space-between px-3 py-2">
+                <v-icon
+                    @click="createTask"
+                    class="btn btn-outline-primary"
+                >
+                mdi-plus
+                </v-icon>
+                <v-icon
+                @click="$emit('delete-section', section.sectionId)"
+                class="btn btn-outline-primary"
+                >
+                mdi-delete
+                </v-icon>
+            </div>
+        </v-card>
+    </div>
+</template>
+
+<script>
+import TaskCard from './TaskCard.vue'
+import {Task} from '/src/task.js'
+
+export default {
+    name: 'TaskSection',
+
+    components : {
+        TaskCard
+    },
+
+    props : {
+        section : Object
+    },
+
+    data(){
+        return {
+            taskId : 1,
+            sectionName : this.section.sectionName
+        }
+    },
+
+    methods : {
+        createTask(){
+            let newTask = new Task(this.section.sectionId, this.taskId, "", "", true, false, false);
+            this.taskId++;
+            this.$emit("create-task", this.section.sectionId, newTask);
+        },
+
+        deleteTask(sectionId, taskId){
+            return this.$emit("delete-task", sectionId, taskId);
+        },
+
+        changeTaskTitle($event, sectionId, taskId){
+            return this.$emit('change-task-title', $event, sectionId, taskId);
+        },
+
+        changeTaskContent($event, sectionId, taskId){
+            return this.$emit('change-task-content', $event, sectionId, taskId);
+        },
+
+        toggleCanEdit(canEdit, sectionId, taskId){
+            return this.$emit("toggle-can-edit", canEdit, sectionId, taskId);
+        },
+
+        toggleIsDone(isDone, sectionId, taskId){
+            return this.$emit("toggle-is-done", isDone, sectionId, taskId);
+        },
+
+        toggleIsFavorite(isFavorite, sectionId, taskId){
+            return this.$emit("toggle-is-favorite", isFavorite, sectionId, taskId);
+        },
+    },
+
+    computed : {
+        currSectionName(){
+            return this.sectionName;
+        }
+    }
+}
+</script>
