f4074f197cab4ee08ead7a2845109144da56b3a9
harutas <96802323+harutas@users.noreply.github.com>
2023-05-20 18:44:01 +0900

    fix: baseurll

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vuetify from 'vite-plugin-vuetify';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vuetify({ styles: 'sass' })],
-  base: '/',
+  base: './',
   build: {
     outDir: resolve(__dirname, 'docs'),
     assetsDir: './',
